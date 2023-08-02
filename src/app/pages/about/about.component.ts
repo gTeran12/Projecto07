3b95d0aafccb096001fc377f672b1ef5c36b8759
gTeran12 <gteran@espol.edu.ec>
2023-08-02 11:32:07 -0400

    Terminada la Guia 19

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  description: string = 'Servicio de Courier... Falta agregar más la descripcion '; /* Cambie el texto con la descripción de su proyecto */
+
+}
